ebb7dc053b5edff8821c56ce824167f21e251b01
Robert De Matteo <robertdematteo@hotmail.com>
2019-07-12 09:53:06 +1000

    changed book to reviews

---
+++ routes/review.routes.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+const controllerMethods = require('../controller/review.controller');
+
+router.get('/review/title/:name', controllerMethods.showReviewByTitle)
+
+router.get('/reviews', controllerMethods.showAllReviews)
+
+router.put('/review/title/:name', controllerMethods.updateReview)
+
+router.delete('/review/title/:title', controllerMethods.deleteReview)
+
+router.post('/seed', controllerMethods.createReview)
+
+module.exports = router
--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 //This will search for the module express in the node-modules and import it in for use in this appliction
 const express = require('express');
-const Book = require('./models/Book');
+const Review = require('./models/Review');
 
 
-const bookRoutes = require('./routes/book.routes');
+const reviewRoutes = require('./routes/review.routes');
 
 
 //Create an app which an instance of express
@@ -18,8 +18,6 @@ const cors = require("cors");
 app.use(cors());
 
 
-// Define the development database
-// const mongoURI = 'mongodb://localhost/bookmarks'
 
 // connecting to mongodb from your application
 mongoose.connect(process.env.DB_URL, { useNewUrlParser: true }, (err) => {
@@ -30,7 +28,7 @@ mongoose.connect(process.env.DB_URL, { useNewUrlParser: true }, (err) => {
 
 app.get('/hello', (req, res) => {
   res.send({
-    hello: 'hello world'
+    hello: 'hello world out there'
   })
 })
 
@@ -39,92 +37,9 @@ app.get('/', (req,res)=> {
 res.send('hello worlds')
 })
 
-
-=======
  // Connection of express routes with root directory
- app.use('/', bookRoutes)
+ app.use('/', reviewRoutes)
 
-// app.get('/', (req,res)=> {
-// res.send('hello world')
-// })
-
-// app.get('/book/title/:name', async (req, res) => {
-//      //console.log("hello")
-//     const { name } = req.params;
-//   console.log(req.params)
-//   //console.log(name)
-//   try {
-//     const myString = name
-//     const findBook = await Book.find({ Title: name})
-//     console.log(findBook)
-//     res.send(findBook);
-//   }
-//   catch(err) { 
-//     console.log('error')
-//       res.json(err);
-//        }
-//   })
-
-//   //Update book
-
-//   app.put('/book/title/:name', async (req, res) => {
-//    const { name } = req.params;
-//    const { newTitle } = req.body
-
-    
-//  try {
-//   const findBook = await Book.findOne({ Title: name})
-//   findBook.Title = newTitle
-//   await findBook.save()
-//   res.send(findBook)
-
-// }
-// catch(err) { 
-//   console.log('error')
-//     res.status(400).json(err);
-//      }
-//  })
-
-// app.post('/seed', async (req, res)=> {
-//   const { books } = req.body
-//   console.log(books)
-//   try {
-//     const newBook = new Book ({
-//       Title: books[0].Title,
-//       Author: books[0].Author,
-//       Review: books[0].Review,
-//       Publisher: books[0].Publisher,
-//       YearPublished: books[0].YearPublished,
-//       Genre: [books[0].Genre],
-//       ISBN: books[0].ISBN,
-//       LinkToBuy:books[0].LinkToBuy,
-//       TopPick: books[0].TopPick,
-//       SeoKeyword: books[0].SeoKeyword,   
-//     })
-//     const savedBook = await newBook.save()
-//     //console.log(savedBook)
-//     res.send(`saved ${savedBook.Title} book in database`)
-//   } catch(err) {
-//     res.status().status(400).send(`${err} problem`)
-//     //console.log(`${err} problem`)
-//   }
-// })
-
-// app.delete('/book/title/:title', async (req,res) => {
-//   const { title } = req.params
-
-//   try {
-//     const doc = await Book.findOneAndDelete({ Title: title})
-//     if(!doc){
-//       res.status(404).send(`No Book ${title} found`)
-//     }
-//     console.log(doc);
-//     res.send(`${doc.Title} deleted from database`)
-//   } catch(err) {
-//   //return res.send(`No Book ${title} found`)
-//    return res.status(400).json(err)
-//   }
-// });
 
 app.listen(port, () => {
   console.log(`listening at http://localhost:${port}`)
