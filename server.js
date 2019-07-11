1ad7bbc22a055a6b7ec8257f2ca36fb01902caa6
Robert De Matteo <robertdematteo@hotmail.com>
2019-07-11 12:47:51 +1000

    basic deployment setup

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 //This will search for the module express in the node-modules and import it in for use in this appliction
 const express = require('express');
 const Book = require('./models/Book');
-console.log(Book)
+
 //Create an app which an instance of express
 const app = new express();
-const port = 5500;
+const port = process.env.Port ||  5500;
 const mongoose = require('mongoose');
 require('dotenv').config();
 const mongoURIENV = process.env.MONGO_URI;
@@ -18,6 +18,13 @@ mongoose.connect(mongoURI, { useNewUrlParser: true }, (err) => {
    if(err) return console.log( `database not connected with ${err}`)
    console.log("connected to mongodb")
  })
+
+app.get('/hello', (req, res) => {
+  res.send({
+    hello: 'hello'
+  })
+})
+
 
 app.get('/', (req,res)=> {
 res.send('hello world')
