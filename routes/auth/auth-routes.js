c952c20d51bdb0941bc84377d0bef77e1fb972bb
Garry <pathakg028@gmail.com>
2019-07-17 21:13:24 +1000

    update password done

--- routes/auth/auth-routes.js
+++ routes/auth/auth-routes.js
@@ -2,12 +2,13 @@
 const express = require('express');
 const router = express.Router();
 //const { register, login } = require('../controllers/auth-controller')
-const { register, login, reset, forgot, forgotPass } = require('../../controller/auth/auth-contoller')
+const { register, login, reset, forgot, forgotPass,resetpass } = require('../../controller/auth/auth-contoller')
 
 router.post('/register', register)
 router.post('/login', login)
 router.post('/reset', reset )
 router.post('/forgot', forgot)
 router.post('/forgotpass', forgotPass)
+router.get('/resetpass', resetpass)
 
 module.exports = router;
