658bf1e6e30880d478fc593b62c6c84ba820e451
Robert De Matteo <robertdematteo@hotmail.com>
2019-07-25 12:56:50 +1000

    dev testing complete

--- routes/review-routes.js
+++ routes/review-routes.js
@@ -7,19 +7,9 @@ const multer = require('multer');
 const storage = multer.memoryStorage()
 const upload = multer({ storage: storage }).single('file');
 
-router.get('/review/title/:name', controllerMethods.showReviewByTitle)
 
+//a route needed here and one moved to protecetd routes
 router.get('/reviews', controllerMethods.showAllReviews)
-//moved to protecetd routes
-
-// router.put('/updateReview', upload, controllerMethods.updateReview)
-//moved to protecetd routes
-
-// router.delete('/deleteReview', controllerMethods.deleteReview)
-//moved to protected routes
-
-// router.post('/createReview', upload, controllerMethods.createReview)
-//createReview put into protected routes
 
 router.post('/testseed', controllerMethods.seedData)
 
